a097c08206c27cc3e04b2fa3806539555ba83c57
Liza Arora <liza.arora@outlook.com>
2022-04-23 21:39:46 +1000

    Basics string code lab commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = 'Liza';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+console.log(welcomeMessage);
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(excitedWelcomeMessage);
+
+// const firstInitial = currentUser[0];
+//const restOfName = currentUser.slice(1);
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}  !`;
+console.log(shortGreeting);
